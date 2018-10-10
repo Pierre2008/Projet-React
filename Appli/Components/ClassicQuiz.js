65aa63eead5c7f9e1deccb2e58ef76c8e19820d5
Pierre2008 <pierre.thiollent76@gmail.com>
2018-10-10 22:06:32 +0200

    feat: début d'ajout des mecaniques du quiz classique => récupération des questions dans le json, possibilité de passer d'une question à l'autre, affichage de la bonne réponse en bas de la view

--- Appli/Components/ClassicQuiz.js
+++ Appli/Components/ClassicQuiz.js
@@ -1,44 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, StatusBar, ScrollView, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import { data } from '../assets/Data/quizData';
 
+let newArray = [];
 
 export default class ClassicScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.questionNumber = 0;
+        quizFinish : false;
+        score: 0;
+
+    const jsonData = data.games.quiz1;
+    newArray = Object.keys(jsonData).map( function(k) { return jsonData[k] });
+    this.state = {
+        question : newArray[this.questionNumber].question,
+        questionNumber : newArray[this.questionNumber].questionNumber,
+        options : newArray[this.questionNumber].options,
+        correctoption : newArray[this.questionNumber].correctoption,
+        countCheck : 0
+        }
+    }
+
+
+    _quizFinish(score) {
+        this.setState({ quizFinish: true, score : score });
+      }
+    
+      next(){
+        if(this.questionNumber < newArray.length-1) {
+          this.questionNumber++;
+          this.setState({ countCheck: 0, question: newArray[this.questionNumber].question, options: newArray[this.questionNumber].options, correctoption : newArray[this.questionNumber].correctoption, questionNumber : newArray[this.questionNumber].questionNumber});
+        } else {
+          this.props.quizFinish(this.score);
+        }
+      }
+
     render() {
         return (
             <View style={styles.mainContainer}>
                 <View style={styles.titleContainer}>
-                    <Text style={styles.questionTitle}>Question n°5</Text>
+                    <Text style={styles.questionTitle}>{this.state.questionNumber}</Text>
                 </View>
 
                 <View style={styles.questionContainer}>
-                    <Text>Web</Text>
-                    <Text>Que signifie le sigle WWW ?</Text>
+                    <Text>{this.state.question}</Text>
                 </View>
                 <View style={styles.answersContainer}>
                     <TouchableOpacity style={styles.answersButtons}>
-                        <Text>World Wide Web</Text>
+                        <Text>{this.state.option1}</Text>
                     </TouchableOpacity>
                     
                     <TouchableOpacity style={styles.answersButtons}> 
-                        <Text>Wild Wide Web</Text>
+                        <Text>{this.state.option2}</Text>
                     </TouchableOpacity>
                     
                     <TouchableOpacity style={styles.answersButtons}> 
-                        <Text>World Wildlife Web</Text>
+                        <Text>{this.state.option3}</Text>
                     </TouchableOpacity>
                     
                     <TouchableOpacity style={styles.answersButtons}>                    
-                        <Text>Ché pô</Text>
+                        <Text>{this.state.option4}</Text>
                     </TouchableOpacity>
                 </View>
 
                 <View style={styles.goodAnswerContainer}>
-                    <Text>Le sigle WWW signifie World Wide Web, ou littéralement la « toile (d'araignée) mondiale</Text>
+                    <Text style={styles.goodAnswerText}>{this.state.correctoption}</Text>
                 </View>
 
-                <View>
-                    <TouchableOpacity style={{backgroundColor: '#4267B2'}}>
-                        <Text style={styles.goodAnswerText}>Suivant</Text>
+                <View style= {{flex: 1}}>
+                    <TouchableOpacity onPress={() => this.next()} style={{backgroundColor: '#4267B2'}}>
+                        <Text>Suivant</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -66,6 +99,7 @@ const styles = StyleSheet.create({
     },
 
     questionContainer: {
+        flex: 1,
         height: 100,
         marginTop: 50,
         width: 300,
@@ -76,19 +110,19 @@ const styles = StyleSheet.create({
     },
 
     answersContainer: {
-        flex: 1,
+        flex: 2,
         marginTop: 50,
         justifyContent: 'space-between',
         alignItems: 'center',
+        
     },
     answersButtons: {
         width: 300,
         height: 50,
-        backgroundColor: '#FFF',
         alignItems: 'center',
         justifyContent: 'center',
-        borderRadius: 5
-        
+        borderRadius: 5,
+        backgroundColor: '#FFF'
         
     },
 
@@ -99,6 +133,5 @@ const styles = StyleSheet.create({
     goodAnswerText: {    
         fontSize: 20,
         fontWeight: 'bold',
-        color: '#FFF',
     }    
 });    
---
+++ Appli/assets/Data/quizData.js
@@ -0,0 +1,67 @@
+export const data = {
+	"games": {
+				
+					"quiz1" : {
+						"question1" : {
+							"questionNumber" :"Question 1", 
+							"correctoption" : "World Wide Web",
+							"options" : {
+							"option1" : "Wild Wide Web",
+							"option2" : "World Wide Web",
+							"option3" : "World Wildlife Web",
+							"option4" : "Ché pô"
+					  },
+					  "question" : "Que signifie le sigle WWW ? "
+					},
+
+					"question2" : {
+						"questionNumber" :"Question 2",
+					  "correctoption" : "option4",
+					  "options" : {
+						"option1" : "Argentina",
+						"option2" : "Thailand",
+						"option3" : "India",
+						"option4" : "United Kingdom"
+					  },
+					  "question" : "Bangkok is the capital of ____ "
+					},
+
+					"question3" : {
+						"questionNumber" :"Question 3",
+					  "correctoption" : "option1",
+					  "options" : {
+						"option1" : "Italy",
+						"option2" : "France",
+						"option3" : "Spain",
+						"option4" : "Greece"
+					  },
+					  "question" : "Paris is the capital of ____"
+					},
+
+					"question4" : {
+						"questionNumber" :"Question 4",
+					  "correctoption" : "option2",
+					  "options" : {
+						"option1" : "Angola",
+						"option2" : "Bahamas",
+						"option3" : "Albania",
+						"option4" : "Malta"
+					  },
+					  "question" : "Tirana is the capital of ____"
+					},
+
+					"question5" : {
+						"questionNumber" :"Question 5",
+					  "correctoption" : "option4",
+					  "options" : {
+						"option1" : "Cameroon",
+						"option2" : "Afghanistan",
+						"option3" : "Burkina Faso",
+						"option4" : "Pakistan"
+					  },
+					  "question" : "Islamabad is the capital of ____"
+					}
+				  }
+
+				}				
+			}
