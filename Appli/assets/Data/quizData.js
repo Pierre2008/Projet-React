fc9264a70e680fcaa3d8d8e36739b58b0dd31b17
Rudy <rudy.logre@viacesi.fr>
2018-10-16 10:40:13 +0200

    premier quizz

--- Appli/assets/Data/quizData.js
+++ Appli/assets/Data/quizData.js
@@ -1,67 +1,66 @@
 export const data = {
-	"games": {
-				
-					"quiz1" : {
-						"question1" : {
-							"questionNumber" :"Question 1", 
-							"correctoption" : "option2",
-							"options" : {
-							"option1" : "Wild Wide Web",
-							"option2" : "World Wide Web",
-							"option3" : "World Wildlife Web",
-							"option4" : "Ché pô"
-					  },
-					  "question" : "Que signifie le sigle WWW ? "
-					},
+    "games": {
 
-					"question2" : {
-						"questionNumber" :"Question 2",
-					  "correctoption" : "option4",
-					  "options" : {
-						"option1" : "Argentina",
-						"option2" : "Thailand",
-						"option3" : "India",
-						"option4" : "United Kingdom"
-					  },
-					  "question" : "Bangkok is the capital of ____ "
-					},
+        "quiz1" : {
+            "question1" : {
+                "questionNumber" :"Question 1",
+                "correctoption" : "option2",
+                "options" : {
+                    "option1" : "Wild Wide Web",
+                    "option2" : "World Wide Web",
+                    "option3" : "World Wildlife Web",
+                    "option4" : "Ché pô"
+                },
+                "question" : "Que signifie le sigle WWW ? "
+            },
 
-					"question3" : {
-						"questionNumber" :"Question 3",
-					  "correctoption" : "option1",
-					  "options" : {
-						"option1" : "Italy",
-						"option2" : "France",
-						"option3" : "Spain",
-						"option4" : "Greece"
-					  },
-					  "question" : "Paris is the capital of ____"
-					},
+            "question2" : {
+                "questionNumber" :"Question 2",
+                "correctoption" : "option3",
+                "options" : {
+                    "option1" : " Choucroute et Saucisses d'Alsace",
+                    "option2" : " Create Simple Samples",
+                    "option3" : " Cascading Style Sheets",
+                    "option4" : "Cascading Simple Samples"
+                },
+                "question" : "Que signifie CSS ?"
+            },
 
-					"question4" : {
-						"questionNumber" :"Question 4",
-					  "correctoption" : "option2",
-					  "options" : {
-						"option1" : "Angola",
-						"option2" : "Bahamas",
-						"option3" : "Albania",
-						"option4" : "Malta"
-					  },
-					  "question" : "Tirana is the capital of ____"
-					},
+            "question3" : {
+                "questionNumber" :"Question 3",
+                "correctoption" : "option2",
+                "options" : {
+                    "option1" : " A réaliser des pages dynamiques",
+                    "option2" : " A ajouter du style aux documents web",
+                    "option3" : " A réaliser des pages dynamiques",
+                    "option4" : "A Rien"
+                },
+                "question" : "A quoi sert le langage CSS ?"
+            },
 
-					"question5" : {
-						"questionNumber" :"Question 5",
-					  "correctoption" : "option4",
-					  "options" : {
-						"option1" : "Cameroon",
-						"option2" : "Afghanistan",
-						"option3" : "Burkina Faso",
-						"option4" : "Pakistan"
-					  },
-					  "question" : "Islamabad is the capital of ____"
-					}
-				  }
+            "question4" : {
+                "questionNumber" :"Question 4",
+                "correctoption" : "option2",
+                "options" : {
+                    "option1" : "section id=sidebar",
+                    "option2" : "sidebar",
+                    "option3" : "details",
+                    "option4" : "aside"
+                },
+                "question" : "Quelle nouvelle balise de section permet de regrouper un contenu tangentiel au contenu principal du document ?"
+            },
 
-				}				
-			}
+            "question5" : {
+                "questionNumber" :"Question 5",
+                "correctoption" : "option3",
+                "options" : {
+                    "option1" : "Internet Explorer 7",
+                    "option2" : "Internet Explorer 8",
+                    "option3" : "Internet Explorer 9",
+                    "option4" : "Internet Explorer 10"
+                },
+                "question" : "À partir de quelle version d'Internet Explorer peut-on utiliser nativement les éléments de section HTML5 (sans hack ou script complémentaire)?"
+            }
+        }
+    }
+}
