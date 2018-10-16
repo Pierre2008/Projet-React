06cf3b8c9567b06e7753ea09b9465b20653f57fe
ReVaStar <xgamev05@gmail.com>
2018-10-16 09:41:28 +0200

    Add Shop, Add Contact

---
+++ Appli/Components/Contact.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button, StatusBar, ScrollView, Dimensions, TouchableOpacity, For } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+export default class Contact extends React.Component {
+
+
+    render() {
+            return (
+
+                <View style={styles.mainContainer}>
+                    <Text style={styles.headerLabel}>Nous Contacter</Text>
+
+                    <View>
+
+                    </View>
+                    <FormLabel>Email</FormLabel>
+                    <FormInput/>
+                    <FormValidationMessage>{'Champs Requis'}</FormValidationMessage>
+                    <FormLabel>Message </FormLabel>
+                    <FormInput/>
+                    <FormValidationMessage>{'Champs Requis'}</FormValidationMessage>
+
+                </View>
+            );qA
+
+
+    }
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        backgroundColor: '#FFCC66',
+    },
+
+    questionTitle: {
+        flex: 1,
+        justifyContent: 'center',
+        fontSize: 17,
+
+    },
+
+    headerLabel: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#4267B2',
+    },
+
+});
